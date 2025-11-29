3553ccd49bf9caf16acf3f6229d790437166787e
menogrey <1299267905@qq.com>
2025-11-29 02:53:03 +0000

    Optimize quant_config testcase.
    Signed-off-by: menogrey <1299267905@qq.com>

--- tests/ut/quantization/test_quant_config.py
+++ tests/ut/quantization/test_quant_config.py
@@ -1,4 +1,3 @@
-from typing import Any
 from unittest.mock import MagicMock, patch
 
 from vllm.attention.layer import Attention
@@ -60,7 +59,7 @@ class TestAscendQuantConfig(TestBase):
 
     @patch('torch.npu.is_available')
     def test_override_quantization_method(self, mock_is_available):
-        # Test when NPU is available
+        # Test when quant_method is None
         mock_is_available.return_value = True
         result = AscendQuantConfig.override_quantization_method(None, None)
         self.assertIsNone(result)
@@ -69,9 +68,14 @@ class TestAscendQuantConfig(TestBase):
             hf_quant_cfg, None)
         self.assertEqual(result, "ascend")
 
+        # Test when NPU is available
+        mock_is_available.return_value = True
+        result = AscendQuantConfig.override_quantization_method({}, None)
+        self.assertEqual(result, ASCEND_QUANTIZATION_METHOD)
+
         # Test when NPU is not available
         mock_is_available.return_value = False
-        result = AscendQuantConfig.override_quantization_method(None, None)
+        result = AscendQuantConfig.override_quantization_method({}, None)
         self.assertIsNone(result)
         hf_quant_cfg = {"quant_method": ""}
         result = AscendQuantConfig.override_quantization_method(
--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -38,8 +38,6 @@ from vllm_ascend.utils import (
     update_cudagraph_capture_sizes, update_default_aclgraph_sizes,
     check_kv_extra_config)
 
-# yapf: enable
-
 if TYPE_CHECKING:
     from vllm.config import ModelConfig, VllmConfig
     from vllm.utils import FlexibleArgumentParser
@@ -112,12 +110,12 @@ class NPUPlatform(Platform):
                 if ASCEND_QUANTIZATION_METHOD not in quant_action.choices:
                     quant_action.choices.append(ASCEND_QUANTIZATION_METHOD)
 
+        from vllm_ascend.quantization.awq.awq import \
+            AWQQuantConfig  # noqa: F401
         from vllm_ascend.quantization.compressed_tensors.compressed_tensors import \
             AscendCompressedTensorsConfig  # noqa: F401
         from vllm_ascend.quantization.quant_config import \
             AscendQuantConfig  # noqa: F401
-        from vllm_ascend.quantization.awq.awq import \
-            AWQQuantConfig  # noqa: F401
 
     @classmethod
     def get_device_capability(cls, device_id: int = 0):
